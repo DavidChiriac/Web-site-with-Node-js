a973da15a5d6b7db51dd5e7b81906a631b8945d7
ChiriacDavid <david.chiriac@student.tuiasi.ro>
2021-05-17 17:07:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,8 @@ app.get('/chestionar', (req, res) => {
 app.get('/autentificare', (req, res) => res.render('autentificare', {mesajEroare: req.cookies.mesajEroare}));
 
 app.post('/verificare-autentificare', (req, res) => {
-	res.cookie('mesajEroare', "")
+	res.cookie('mesajEroare', "");
+	res.cookie('utilizator', "");
 	console.log(req.body)
 	if(req.body.username=="admin" && req.body.password=="admin"){
 		res.cookie('utilizator', req.body.username);
